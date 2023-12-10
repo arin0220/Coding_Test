4427f6d5b9452117cae42cf7654bde403a402444
arin0220 <127179215+arin0220@users.noreply.github.com>
2023-12-10 14:56:29 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, n) {
+    var answer = '';
+    const arr = my_string.split("");
+    for(let i=0; i<arr.length; i++){    
+        for(let j=0; j<n; j++){
+            answer += arr[i];
+        }
+    } 
+    return answer;
+}
