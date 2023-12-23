dee0b97bd012f1dd43268fd907f977a5140bec48
arin0220 <127179215+arin0220@users.noreply.github.com>
2023-12-23 17:19:02 +0900

    [level unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,11 @@
+function solution(myString) {
+    var answer = '';
+    for(var i=0; i<myString.length; i++){
+        if(myString[i] == 'A' || myString[i] == 'a'){
+            answer += myString[i].toUpperCase();
+        }else{
+            answer += myString[i].toLowerCase();
+        }
+    }
+    return answer;
+}
